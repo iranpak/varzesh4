4784b67d8b180d4cbc0e65a073c44a7a014ad9d3
MohammadHossein Iranpak <mh.iranpak1997@gmail.com>
2019-02-01 23:50:57 +0330

    league page backend 2

--- v4_league/models.py
+++ v4_league/models.py
@@ -7,29 +7,29 @@ from django.dispatch import receiver
 class League(models.Model):
     name = models.CharField(max_length=64)
     teams = models.ManyToManyField(Team, related_name='teams')
-    year = models.IntegerField(validators=[MinValueValidator(1320), MaxValueValidator(1500)])
-
-    def get_league_fullname(self):
-        return self.name + ' - ' + str(self.year)
+    year = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name
+        return self.name + ' ' + self.year
+
+    class Meta:
+        unique_together = ('name', 'year')
 
 
-class TeamsOfLeague(models.Model):
-    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league')
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team')
-    team_score = models.IntegerField(default=0)
-    team_difference = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.league.name + '-' + self.team.persian_name
-
-
-def create_teams_of_league_models(instance, **kwargs):
-    for team in instance.teams.all():
-        record = TeamsOfLeague.objects.create(league=instance, team=team)
-        record.save()
-
-
-models.signals.m2m_changed.connect(create_teams_of_league_models, sender=League.teams.through)
+# class TeamsOfLeague(models.Model):
+#     league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league')
+#     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team')
+#     team_score = models.IntegerField(default=0)
+#     team_difference = models.IntegerField(default=0)
+#
+#     def __str__(self):
+#         return self.league.name + '-' + self.team.persian_name
+#
+#
+# def create_teams_of_league_models(instance, **kwargs):
+#     for team in instance.teams.all():
+#         record = TeamsOfLeague.objects.create(league=instance, team=team)
+#         record.save()
+#
+#
+# models.signals.m2m_changed.connect(create_teams_of_league_models, sender=League.teams.through)
--- v4_league/views.py
+++ v4_league/views.py
@@ -3,6 +3,8 @@ from .models import League
 from v4_match.models import Match
 from django.db.models import Q
 from operator import attrgetter
+import traceback
+
 
 # from v4
 
@@ -30,18 +32,52 @@ def show_league_page(request, league_id):
     for team in teams_models:
         team_matches = get_team_matches(team, league)
         winner_data = calculate_number_of_win_and_lose(team, team_matches)
-        team_score = winner_data['win']*3 + winner_data['tie']
+        team_score = winner_data['win'] * 3 + winner_data['tie']
         team_goals = calculate_number_of_goals(team, team_matches)
         team_model = TeamModel(team, len(team_matches), winner_data['win'], winner_data['lose'], winner_data['tie'],
                                team_goals['scored_goals'], team_goals['received_goals'], team_score)
         teams.append(team_model)
 
+    [current_week_matches, next_week_matches, week] = get_current_week_matches(league)
+
     teams.sort(key=attrgetter('score', 'goal_difference'), reverse=True)
-    return render(request, 'v4_league/league_page.html', {'league': league, 'teams': teams})
+    return render(request, 'v4_league/league_page.html',
+                  {'league': league, 'teams': teams, 'current_week_matches': current_week_matches,
+                   'next_week_matches': next_week_matches, 'week': week})
+
+
+def get_current_week_matches(league):
+    done_matches = Match.objects.filter(home_score__isnull=False, league=league)
+    done_matches_list = []
+    for match in done_matches:
+        done_matches_list.append(match)
+
+    done_matches_list.sort(key=attrgetter('week'), reverse=True)
+
+    counter = 0
+    last_match_week = done_matches_list[0].week
+    print(last_match_week)
+    for match in done_matches_list:
+        if match.week == last_match_week:
+            counter += 1
+        else:
+            break
+
+    print(counter)
+
+    number_of_league_teams = league.teams.all().count()
+
+    if counter == number_of_league_teams / 2:
+        return [Match.objects.filter(league=league, week=last_match_week + 1),
+                Match.objects.filter(league=league, week=last_match_week + 2), last_match_week + 1]
+    else:
+        return [Match.objects.filter(league=league, week=last_match_week),
+                Match.objects.filter(league=league, week=last_match_week + 1), last_match_week]
 
 
 def get_team_matches(team, league):
-    team_matches = Match.objects.filter((Q(home=team) | Q(away=team)) & Q(league=league)).all()
+    team_matches = Match.objects.filter(
+        ((Q(home=team) | Q(away=team)) & Q(league=league)) & Q(home_score__isnull=False)).all()
     return team_matches
 
 
@@ -80,3 +116,20 @@ def calculate_number_of_goals(team, team_matches):
             scored_goals += match.away_score
 
     return {'scored_goals': scored_goals, 'received_goals': received_golas}
+
+
+def search_for_league(request):
+    key = request.GET.get('key', '')
+    search_keys = key.split()
+    leagues = League.objects.all()
+    leagues = [l for l in leagues if l.__str__() == key]
+    if len(leagues) == 1:
+        return render(request, 'v4_league/search_league.html', {'leagues': leagues})
+    else:
+        leagues = League.objects.filter(name__contains=search_keys[0])
+        if len(search_keys) == 2:
+            leagues = [l for l in leagues if l.year.__contains__(search_keys[1])]
+        for league in leagues:
+            print(league)
+
+        return render(request, 'v4_league/search_league.html', {'leagues': leagues})
