3a3b737a6509ccfc36a7721351d63295e0026307
MohammadHossein Iranpak <mh.iranpak1997@gmail.com>
2019-02-01 11:13:28 +0330

    league page backend

--- v4_league/models.py
+++ v4_league/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.core.validators import *
+from v4_team.models import Team
+from django.dispatch import receiver
 
 
 class League(models.Model):
     name = models.CharField(max_length=64)
-    number_of_teams = models.IntegerField()
+    teams = models.ManyToManyField(Team, related_name='teams')
     year = models.IntegerField(validators=[MinValueValidator(1320), MaxValueValidator(1500)])
 
     def get_league_fullname(self):
         return self.name + ' - ' + str(self.year)
+
+    def __str__(self):
+        return self.name
+
+
+class TeamsOfLeague(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team')
+    team_score = models.IntegerField(default=0)
+    team_difference = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.league.name + '-' + self.team.persian_name
+
+
+def create_teams_of_league_models(instance, **kwargs):
+    for team in instance.teams.all():
+        record = TeamsOfLeague.objects.create(league=instance, team=team)
+        record.save()
+
+
+models.signals.m2m_changed.connect(create_teams_of_league_models, sender=League.teams.through)
